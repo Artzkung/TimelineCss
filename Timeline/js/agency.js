3fa8c64a123c460976168d215184ea986060b02c
Artzkung <nattapongartkan@gmail.com>
2017-08-11 09:49:19 +0700

    Before Done
    Waiting Create BoxProfile

---
+++ Timeline/js/agency.js
@@ -0,0 +1,14 @@
+$('body').scrollspy({
+  target: '#sidebar-wrapper'
+})
+$(".sidebar-nav a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+      window.location.hash = hash;
+    });
+  }
+});
